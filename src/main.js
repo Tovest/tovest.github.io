a2cb29dfed56c49b263ea26c06da4fe66c3fe110
Tovest <94750683+Tovest@users.noreply.github.com>
2021-11-20 20:33:27 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import { hello , PointObj , LineObj , PolygonObj } from "./obj_module.js";
 
 console.log("v8");
 
-console.log(new LineObj(new PointObj(1,2,3), new PointObj(4,5,6)).getInfoTree().toString())
+console.log(new LineObj(new PointObj(1,2,3), new PointObj(4,5,6)).getInfoTree().toText())
