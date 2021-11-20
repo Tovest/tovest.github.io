979087eba6c7cf1cff569430ab1957d2bea7abc1
Tovest <94750683+Tovest@users.noreply.github.com>
2021-11-21 00:17:39 +0100

    Create camera_module.js

---
+++ src/camera_module.js
@@ -0,0 +1,11 @@
+export class Camera {
+	constructor(x,y,z,dirX,dirY,dirZ,fov) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.dirX = dirX;
+		this.dirY = dirY;
+		this.dirZ = dirZ;
+		this.fov = fov;
+	}
+}
