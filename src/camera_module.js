9b60e9296efde841acf1a64c6f8f789c65d86233
Tovest <94750683+Tovest@users.noreply.github.com>
2021-11-22 00:36:35 +0100

    Update camera_module.js

--- src/camera_module.js
+++ src/camera_module.js
@@ -1,11 +1,25 @@
 export class Camera {
-	constructor(x,y,z,dirX,dirY,dirZ,fov) {
+	constructor(x,y,z,dirX,dirY,dirZ) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 		this.dirX = dirX;
 		this.dirY = dirY;
 		this.dirZ = dirZ;
-		this.fov = fov;
+	}
+	translateToElements2d(objList) {
+		return [];
+	}
+}
+
+export class CameraOrtho {
+	constructor(x,y,z,dirX,dirY,dirZ) {
+		super(x,y,z,dirX,dirY,dirZ);
+	}
+	translateToElements2d(objList) {
+		var elements2d = [];
+		for (int i=0; i<objList.length; i++) {
+			elements2d.push( ...(objList[i].toElements2d(this)) );
+		}
 	}
 }
