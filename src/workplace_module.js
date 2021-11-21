2cf93c722e05dcc5567cfe059dc0442447f58920
Tovest <94750683+Tovest@users.noreply.github.com>
2021-11-21 14:55:23 +0100

    Update workplace_module.js

--- src/workplace_module.js
+++ src/workplace_module.js
@@ -1,14 +1,20 @@
 import { Camera } from "./camera_module.js"
 
-export class Workplace {
-	constructor() {
-		
-	}
-}
-
 export class World {
 	constructor() {
 		this.objList = [];
-		this.camera = new Camera(0,0,0,0,1,0,90);
+	}
+}
+
+export class View {
+	constructor() {
+		this.world = undefined;
+	}
+}
+
+export class Workplace {
+	constructor() {
+		//this.options = undefined; //Not yet, ToDo
+		//this.
 	}
 }
