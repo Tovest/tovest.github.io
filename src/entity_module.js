7ca5bec7fe3752edd1488fecd73780750f2d0cae
Tovest <94750683+Tovest@users.noreply.github.com>
2021-11-21 15:48:26 +0100

    Create entity_module.js

---
+++ src/entity_module.js
@@ -0,0 +1,10 @@
+import { View } from "./workplace_modeule.js"
+
+export class entity {
+	constructor() {
+		this.body = []; //Array of obj from obj_module
+	}
+	render(view) {
+		return undefined //ToDo
+	}
+}
