0236e9b570c0451d67c07ecddeb4269216c473c0
Tovest <94750683+Tovest@users.noreply.github.com>
2021-11-24 00:28:23 +0100

    Update selectableArea_module.js

--- src/selectableArea_module.js
+++ src/selectableArea_module.js
@@ -1,9 +1,13 @@
-import { PointArea , LineArea , TriangleArea } from "./area_module.js"
-
 export class SelectableArea {
 	constructor(area) {
 		this.area = area;
 		this.onHover = undefined;
 		this.onClick = undefined;
+	}
+	notifyHover(x,y,canvas) {
+		this.onHover.execute(this,x,y,canvas);
+	}
+	notifyClick(x,y,canvas) {
+		this.onClick.execute(this,x,y,canvas);
 	}
 }
