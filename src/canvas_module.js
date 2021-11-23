d760534865aca4f50e783c66bef8ca09eabc79b8
Tovest <94750683+Tovest@users.noreply.github.com>
2021-11-23 22:59:11 +0100

    Update canvas_module.js

--- src/canvas_module.js
+++ src/canvas_module.js
@@ -3,4 +3,11 @@ export class Canvas {
 		this.selectableAreas = [];
 		this.elements2d = [];
 	}
+	onHover(x,y) {
+		for (let i=0; i<this.selectableAreas.length; i++) {
+			if (this.selectableAreas[i].containsPoint(x,y)) {
+				this.selectableAreas[i].notifyHover(x,y);
+			}
+		}
+	}
 }
