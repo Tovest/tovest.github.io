84c00f3b654fd8ccbf65a51aa33bd6b0e9c7b23d
Tovest <94750683+Tovest@users.noreply.github.com>
2021-11-25 02:11:02 +0100

    Update entity_module.js

--- src/entity_module.js
+++ src/entity_module.js
@@ -1,9 +1,20 @@
+import { PointObj } from "./obj_module.js"
+
 export class entity {
-	constructor() {
-		this.body = []; //Array of obj from obj_module
-	}
+	constructor() {}
 	translateToCanvas(camera,canvas) {
 		//ToDo, this should setup canvas with elements2d and selectable areas.
 		return;
 	}
+}
+export class snapPoint {
+	constructor(x,y,z) {
+		this.point = new PointObj(x,y,z);
+	}
+	translateToCanvas(camera,canvas) {
+		canvas.addElement2d()
+		canvas.addSelectableArea()
+		return;
+	}
+	
 }
