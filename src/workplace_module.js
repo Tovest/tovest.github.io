aab0aa8580612780814d5d22c00720fcca8011be
Tovest <94750683+Tovest@users.noreply.github.com>
2021-11-25 15:38:59 +0100

    Update workplace_module.js

--- src/workplace_module.js
+++ src/workplace_module.js
@@ -1,10 +1,10 @@
-import { Camera } from "./camera_module.js";
+import { CameraOrtho } from "./camera_module.js";
 import { Canvas } from "./canvas_module.js";
 
 export class Workplace {
 	constructor(canvas) {
 		this.entityList = [];
-		this.activeCamera = new Camera(0,0,0,0,1,0);
+		this.activeCamera = new CameraOrtho(0,0,0,0,1,0);
 		this.canvas = canvas;
 	}
 	render() {
