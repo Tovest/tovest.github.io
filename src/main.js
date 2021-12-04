5ddb9e9ea55081dbb27d441459a149bdae0f37b3
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-04 16:25:31 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import { SnapPoint } from "./entity_module.js"
 console.log("v12");
 
 var what = 5;
-var point = new SnapPoint(0,0,0);
-var point2 = new SnapPoint(0,0,15);
+var point = new SnapPoint(0,20,0);
+var point2 = new SnapPoint(0,20,15);
 var canvas = new Canvas();
 var workplace = new Workplace(new CameraOrtho(0,0,0,0,0,0), canvas);
 workplace.entityList.push(point);
