abc28678e03a1143c9965460f3973b79d34470c7
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-04 13:20:23 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { PointObj , LineObj , PolygonObj } from "./obj_module.js";
+import { Canvas } from "./canvas_module.js"
+import { Workplace } from "./workplace_module.js"
+import { CameraOrtho } from "./camera_module.js"
 
-console.log("v9");
+console.log("v10 hope");
 
+
+var canvas = new Canvas();
+var workplace = new Workplace(new CameraOrtho(0,0,0,0,0,0), canvas);
 console.log(new LineObj(new PointObj(1,2,3), new PointObj(4,5,6)).getInfoTree().toText())
