2df81b84af1ccbff0c44ea9f6cc48bdc9157ed7f
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-04 12:53:11 +0100

    Update event_module.js

--- src/event_module.js
+++ src/event_module.js
@@ -8,11 +8,20 @@ export class event {
 	}
 }
 
-export class eventAddSprite {
+export class eventAddSprite extends event {
 	constructor(image) {
 		this.image = image;
 	}
 	execute(requestingArea,x,y,canvas) {
 		canvas.addElement2d(new Sprite(x,y.this.image));
+	}
+}
+
+export class eventConsoleLog extends event {
+	constructor(message) {
+		this.message = message;
+	}
+	execute(requestingArea,x,y,canvas) {
+		console.log(this.message,x,y);
 	}
 }
