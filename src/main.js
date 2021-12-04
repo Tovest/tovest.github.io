856f15f139b67ad64f10ac488473e816262824dd
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-04 14:04:58 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import { Canvas } from "./canvas_module.js"
 import { Workplace } from "./workplace_module.js"
 import { CameraOrtho } from "./camera_module.js"
 
-console.log("v10 hope");
+console.log("v11");
 
 
 var canvas = new Canvas();
 var workplace = new Workplace(new CameraOrtho(0,0,0,0,0,0), canvas);
+canvas.triggerHover(5,5);
 console.log(new LineObj(new PointObj(1,2,3), new PointObj(4,5,6)).getInfoTree().toText())
