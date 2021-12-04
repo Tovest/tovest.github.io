00f720a0118dbffb263a09c4481dd27ef8350d7c
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-04 18:36:19 +0100

    Update canvas_module.js

--- src/canvas_module.js
+++ src/canvas_module.js
@@ -3,6 +3,7 @@ export class Canvas {
 		this.selectableAreas = [];
 		this.elements2d = [];
 		this.canvasElement = canvasElement;
+		this.canvasElement.onmouseover = function(e){triggerHover(e.offsetX,e.offsetY)};
 	}
 	triggerHover(x,y) {
 		for (let i=0; i<this.selectableAreas.length; i++) {
@@ -11,7 +12,6 @@ export class Canvas {
 			}
 		}
 	}
-	console.log("is this working?");
 	addSelectableArea(selectableArea) {
 		this.selectableAreas.push(selectableArea);
 	}
