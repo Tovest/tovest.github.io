3aec166db3124f0dab0f943820110f9f55573281
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-04 15:00:21 +0100

    Update entity_module.js

--- src/entity_module.js
+++ src/entity_module.js
@@ -2,6 +2,7 @@ import { PointObj } from "./obj_module.js"
 import { PointArea } from "./area_module.js"
 import { SelectableArea } from "./selectableArea_module.js"
 import { Vector3d } from "./vector.js"
+import { EventConsoleLog } from "./event_module.js"
 
 export class Entity {
 	constructor() {}
@@ -18,8 +19,8 @@ export class SnapPoint extends Entity {
 	translateToCanvas(camera,canvas) {
 		var coords = camera.getScreenCoordsOfPoint(this.point.x,this.point.y,this.point.z);
 		var selectableArea = new SelectableArea(new PointArea(coords.x,coords.y,5));
-		selectableArea.onHover = new eventConsoleLog("Hovering Snap Point");
-		selectableArea.onClick = new eventConsoleLog("Clicked Snap Point");
+		selectableArea.onHover = new EventConsoleLog("Hovering Snap Point");
+		selectableArea.onClick = new EventConsoleLog("Clicked Snap Point");
 		canvas.addSelectableArea(selectableArea);
 	}
 }
