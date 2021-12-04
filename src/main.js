d9094eae789ee3f743c445f5445c392833056b81
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-04 15:54:02 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,15 +8,25 @@ console.log("v12");
 
 var what = 5;
 var point = new SnapPoint(0,0,0);
+var point = new SnapPoint(0,0,15);
 var canvas = new Canvas();
 var workplace = new Workplace(new CameraOrtho(0,0,0,0,0,0), canvas);
 workplace.entityList.push(point);
 console.log(workplace);
 workplace.render();
-canvas.triggerHover(5,6);
-canvas.triggerHover(5,3);
-canvas.triggerHover(5,0.1);
-canvas.triggerHover(5,0);
-canvas.triggerHover(5,-0.1);
-canvas.triggerHover(5,-0.3);
-canvas.triggerHover(5,-6);
+console.log(0,0);
+canvas.triggerHover(0,0);
+console.log(0,-6)
+canvas.triggerHover(0,-6);
+console.log(0,-2)
+canvas.triggerHover(0,-2);
+console.log(0,2)
+canvas.triggerHover(0,2);
+console.log(0,7)
+canvas.triggerHover(0,7);
+console.log(0,10)
+canvas.triggerHover(0,10);
+console.log(0,14)
+canvas.triggerHover(0,14);
+console.log(0,21)
+canvas.triggerHover(0,21);
