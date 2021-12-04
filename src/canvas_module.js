46fd04d1a9c312ca76cd99d089f7ab547efccdbc
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-04 18:24:08 +0100

    Update canvas_module.js

--- src/canvas_module.js
+++ src/canvas_module.js
@@ -11,7 +11,7 @@ export class Canvas {
 			}
 		}
 	}
-	this.canvasElement.onmouseover = function(e) {console.log("test",e.offsetX)};
+	console.log(this.canvasElement);
 	addSelectableArea(selectableArea) {
 		this.selectableAreas.push(selectableArea);
 	}
