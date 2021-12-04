a09f12ff5b9785f71f08cf3a40cf6315883cec8f
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-04 14:25:22 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import { PointObj , LineObj , PolygonObj } from "./obj_module.js";
 import { Canvas } from "./canvas_module.js"
 import { Workplace } from "./workplace_module.js"
 import { CameraOrtho } from "./camera_module.js"
+import { SnapPoint } from "./entity_module"
 
-console.log("v11");
+console.log("v12");
 
-
+var what = 5;
+var point = new SnapPoint(0,0,0);
 var canvas = new Canvas();
 var workplace = new Workplace(new CameraOrtho(0,0,0,0,0,0), canvas);
 canvas.triggerHover(5,5);
-console.log(new LineObj(new PointObj(1,2,3), new PointObj(4,5,6)).getInfoTree().toText())
