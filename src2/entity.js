4f23a3247bacaee98d1e0af81f76013038957e11
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-05 20:11:23 +0100

    Update entity.js

--- src2/entity.js
+++ src2/entity.js
@@ -1,5 +1,20 @@
-class SnapPoint {
+class Entity {
+	constructor() {}
+	translateToCanvas(camera,canvas) {
+		console.log(this, ">>> this entity has not defined the translateToCanvas function");
+	}
+}
+
+class SnapPoint extends Entity {
 	constructor(x,y,z) {
-		this.point = new Point(x,y,z)
+		super();
+		this.point = new Point(x,y,z);
+	}
+	translateToCanvas(camera,canvas) {
+		var coords = camera.getScreenCoordsOfPoint(this.point.x,this.point.y,this.point.z);
+		var selectableArea = new SelectableArea(new PointArea(coords.x,coords.y,5));
+		selectableArea.onHover = new EventConsoleLog("Hovering Snap Point");
+		selectableArea.onClick = new EventConsoleLog("Clicked Snap Point");
+		canvas.addSelectableArea(selectableArea);
 	}
 }
