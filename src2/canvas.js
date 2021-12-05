66e555a70ab9e06f91d85f7e16d54dc47b53047a
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-05 21:36:36 +0100

    Update canvas.js

--- src2/canvas.js
+++ src2/canvas.js
@@ -1,11 +1,33 @@
 class Canvas {
 	constructor(canvasElement, camera) {
+		this.selectableAreas = [];
+		this.elements2d = [];
 		this.canvasElement = canvasElement;
 		this.activeCamera = camera;
 	}
 	render(entityList) {
 		for (var entity in entityList) {
 			entity.translateToCanvas(this.activeCamera,this);
+		}
+	}
+	addSelectableArea(selectableArea) {
+		this.selectableAreas.push(selectableArea);
+	}
+	addElement2d(element2d) {
+		this.element2d.push(element2d);
+	}
+	triggerHover(x,y) {
+		for (var selectableArea in this.selectableAreas) {
+			if (selectableArea.area.containsPoint(x,y)) {
+				selectableArea.notifyHover(x,y,this);
+			}
+		}
+	}
+	triggerClick(x,y) {
+		for (var selectableArea in this.selectableAreas) {
+			if (selectableArea.area.containsPoint(x,y)) {
+				selectableArea.notifyClick(x,y,this);
+			}
 		}
 	}
 }
