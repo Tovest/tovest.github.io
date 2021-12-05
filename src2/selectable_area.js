40381f8f81ad1203b722a147c3c32171f4d8eea1
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-05 20:31:01 +0100

    Create selectable_area.js

---
+++ src2/selectable_area.js
@@ -0,0 +1,15 @@
+export class SelectableArea {
+	constructor(area) {
+		this.area = area;
+		this.onHover = undefined;
+		this.onClick = undefined;
+	}
+	notifyHover(x,y,canvas) {
+		var point = this.area.getCorrispondingPoint(x,y);
+		this.onHover.execute(this,point.x,point.y,canvas);
+	}
+	notifyClick(x,y,canvas) {
+		var point = this.area.getCorrispondingPoint(x,y);
+		this.onClick.execute(this,point.x,point.y,canvas);
+	}
+}
