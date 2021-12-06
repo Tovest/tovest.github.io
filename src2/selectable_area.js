f7565a3945955b82e733d5cc9a7bf7031a67e015
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-06 01:25:33 +0100

    Update selectable_area.js

--- src2/selectable_area.js
+++ src2/selectable_area.js
@@ -1,15 +1,100 @@
+function distance(x1,y1,x2,y2) {
+	return Math.sqrt(
+		(x1-x2)*(x1-x2)+
+		(y1-y2)*(y1-y2)
+	)
+}
+
+function sideOfLineUnormalized(x1,y1,x2,y2,x3,y3) {
+	return (x2-x1)*(y1-y3)-(y2-y1)*(x1-x3)
+}
+
 class SelectableArea {
 	constructor(area) {
-		this.area = area;
 		this.onHover = undefined;
 		this.onClick = undefined;
 	}
-	notifyHover(x,y,canvas) {
-		var point = this.area.getCorrispondingPoint(x,y);
-		this.onHover.execute(this,point.x,point.y,canvas);
+	containsPoint(x,y) {
+		return false;
 	}
-	notifyClick(x,y,canvas) {
-		var point = this.area.getCorrispondingPoint(x,y);
-		this.onClick.execute(this,point.x,point.y,canvas);
+	getCorrispondingPoint(x,y) {
+		return undefined;
+	}
+	executeOnHover(x,y,canvas) {}
+	executeOnClick(x,y,canvas) {}
+}
+
+class PointArea extends SelectableArea {
+	constructor(x,y,radius,corrispongingVertex) {
+		super();
+		this.centerX = x;
+		this.centerY = y;
+		this.radius = radius;
+		this.corrispongingVertex = corrispongingVertex;
+	}
+	containsPoint(x,y) {
+		var dist = distance(this.centerX,this.centerY,x,y);
+		return (dist <= this.radius);
+	}
+	getCorrispondingVertex(x,y) {
+		return this.corrispongingVertex;
+	}
+	executeOnHover(x,y,canvas) {
+		if (this.containsPoint(x,y)) this.onHover.execute(this,this.corrispongingVertex,canvas);
+	}
+	executeOnClick(x,y,canvas) {
+		if (this.containsPoint(x,y)) this.onClick.execute(this,this.corrispongingVertex,canvas);
+	}
+}
+
+class LineArea extends SelectableArea {
+	constructor(x1,y1,x2,y2,width,corrispongingVertex1,corrispongingVertex2) {
+		super();
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.width = width;
+		this.corrispongingVertex1 = corrispongingVertex1;
+		this.corrispongingVertex2 = corrispongingVertex2;
+	}
+	containsPoint(x,y) {
+		var length = distance(this.x1,this.y1,this.x2,this.y2);
+		var distanceFromLine =
+		    Math.abs((this.x2-this.x1)*(this.y1-y)-(this.y2-this.y1)*(this.x1-x))/length;
+		var distanceFromPerpendicularBisector =
+		    Math.abs((this.y1-this.y2)*((this.y1+this.y2)/2.0-y)-(this.x1-this.x2)*((this.x1+this.x2)/2.0-x))/length;
+		return (distanceFromLine <= this.width && distanceFromPerpendicularBisector <= length/2.0);
+	}
+	getCorrispondingPoint(x,y) {
+		return undefined; //ToDo
+	}
+	executeOnHover(x,y,canvas) {
+		if (this.containsPoint(x,y)) this.onHover.execute(this,new VertexMidpoint(this.corrispongingVertex1,this.corrispongingVertex2),canvas);
+	}
+	executeOnClick(x,y,canvas) {
+		if (this.containsPoint(x,y)) this.onClick.execute(this,new VertexMidpoint(this.corrispongingVertex1,this.corrispongingVertex2),canvas);
+	}
+}
+
+class TrisArea extends SelectableArea {
+	constructor(x1,y1,x2,y2,x3,y3) {
+		super();
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.x3 = x3;
+		this.y3 = y3;
+	}
+	containsPoint(x,y) {
+		return (
+			( sideOfLineUnormalized(this.x1,this.y1,this.x2,this.y2,x,y) >= 0 ) ==
+			( sideOfLineUnormalized(this.x2,this.y2,this.x3,this.y3,x,y) >= 0 ) ==
+			( sideOfLineUnormalized(this.x3,this.y3,this.x1,this.y1,x,y) >= 0 )
+		);
+	}
+	getCorrispondingPoint(x,y) {
+		return new Vector2d(x,y);
 	}
 }
