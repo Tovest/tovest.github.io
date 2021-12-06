2d1ede46098735ae6c13c1050a5f1ee06f03625e
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-06 10:29:43 +0100

    Update vertex.js

--- src2/vertex.js
+++ src2/vertex.js
@@ -53,6 +53,7 @@ class VertexMidpoint extends Vertex {
 
 class VertexLinePoint extends Vertex {
 	constructor(v1,v2,proportion) {
+		this.proportion = proportion;
 		super( (v1.x+v2.x)*proportion , (v1.y+v2.y)*proportion , (v1.z+v2.z)*proportion );
 		v1.observers.push(this);
 		v2.observers.push(this);
