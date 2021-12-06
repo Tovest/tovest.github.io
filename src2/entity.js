14b45c5117ac9354e8f585fbcab773cac2db2896
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-06 11:19:15 +0100

    Update entity.js

--- src2/entity.js
+++ src2/entity.js
@@ -11,8 +11,26 @@ class SnapPoint extends Entity {
 		this.point = new VertexEdge(x,y,z);
 	}
 	translateToCanvas(camera,canvas) {
-		var coords = camera.getScreenCoordsOfVertex(this.point);
-		var selectableArea = new PointArea(coords.x,coords.y,5,this.point);
+		var screenCoords = camera.getScreenCoordsOfVertex(this.point);
+		var selectableArea = new PointArea(screenCoords.x,screenCoords.y,5,this.point);
+		selectableArea.onHover = new EventConsoleLog("Hovering Snap Point");
+		selectableArea.onClick = new EventConsoleLog("Clicked Snap Point");
+		canvas.addSelectableArea(selectableArea);
+	}
+}
+
+class Line extends Entity {
+	constructor(x1,y1,z1,x2,y2,z2) {
+		super();
+		this.endpoint1 = new VertexEdge(x1,y1,z1);
+		this.endpoint2 = new VertexEdge(x2,y2,z2);
+		this.middlepoint = new VertexMidpoint(this.endpoint1,this.endpoint2);
+	}
+	translateToCanvas(camera,canvas) {
+		var screenCoordsEndpoint1 = camera.getScreenCoordsOfVertex(this.endpoint1);
+		var screenCoordsEndpoint2 = camera.getScreenCoordsOfVertex(this.endpoint2);
+		var selectableAreaEndpoint1 = new PointArea(screenCoordsEndpoint1.x,screenCoordsEndpoint1.y,5,this.endpoint1);
+		var selectableAreaEndpoint2 = new PointArea(screenCoordsEndpoint2.x,screenCoordsEndpoint2.y,5,this.endpoint2);
 		selectableArea.onHover = new EventConsoleLog("Hovering Snap Point");
 		selectableArea.onClick = new EventConsoleLog("Clicked Snap Point");
 		canvas.addSelectableArea(selectableArea);
