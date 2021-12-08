57a3659835a51165c02af495d3eafd43458ec7aa
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-08 12:11:51 +0100

    Update canvas.js

--- src2/canvas.js
+++ src2/canvas.js
@@ -1,23 +1,26 @@
 class Canvas {
-	constructor(canvasElement, camera) {
-		this.selectableAreas = [];
-		this.elements2d = [];
-		this.canvasElement = canvasElement;
+	constructor(canvasID,camera) {
+		this.canvasElement = document.getElementById(canvasID);
 		this.camera = camera;
+		this.selectableAreas = [];
+		this.drawingElements = [];
 	}
 	render(entityList) {
 		this.selectableAreas = [];
-		this.elements2d = [];
+		this.drawingElements = [];
 		for (var i=0; i<entityList.length; i++) {
 			entityList[i].translateToCanvas(this);
 		}
 		//// this.triggerHover(x,y);
+		for (var i=0; i<this.elements2d.length; i++) {
+			this.elements2d[i].requestDrawTo(this);
+		}
 	}
 	addSelectableArea(selectableArea) {
 		this.selectableAreas.push(selectableArea);
 	}
-	addElement2d(element2d) {
-		this.element2d.push(element2d);
+	addDrawingElement(drawingElement) {
+		this.drawingElements.push(drawingElement);
 	}
 	triggerHover(x,y) {
 		for (var i=0; i<this.selectableAreas.length; i++) {
@@ -32,5 +35,35 @@ class Canvas {
 				this.selectableAreas[i].onClick.execute();
 			}
 		}
+	}
+	drawPoint(point) {
+		console.log("This canvas can't draw Points");
+	}
+	drawLine(line) {
+		console.log("This canvas can't draw Lines");
+	}
+	drawTriangle(triangle) {
+		console.log("This canvas can't draw Triangles");
+	}
+}
+
+class Canvas2d extends Canvas {
+	constructor(canvasID,camera) {
+		super(canvasID,camera);
+		this.ctx = this.canvasElement.getContext("2d");
+	}
+	drawPoint(point) {
+		var w = this.canvasElement.width/2;
+		var h = this.canvasElement.height/2;
+		this.ctx.fillStyle = point.color;
+		this.ctx.fillRect(w+point.x-2, h+point.y-2, w+point.x+2, h+point.y+2);
+	}
+	drawLine(line) {
+		var w = this.canvasElement.width/2;
+		var h = this.canvasElement.height/2;
+		this.ctx.moveTo(w+line.x1, h+line.y1);
+		this.ctx.lineTo(w+line.x2, h+line.y2);
+		this.ctx.fillStyle = line.color;
+		this.ctx.stroke();
 	}
 }
