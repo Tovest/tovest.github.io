2126abcdc618dc3c4b238c7f3725dc63e21d3f73
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-08 12:42:29 +0100

    Update main.js

--- src2/main.js
+++ src2/main.js
@@ -1,5 +1,5 @@
 var canvas = new Canvas(undefined, new CameraOrtho(0,0,0,0,0,0));
-var snapPoint = SnapPoint.create(0,20,0);
+var snapPoint = SnapPointEntity.create(0,20,0);
 var workplace = new Workplace([snapPoint],canvas);
 
 workplace.render();
