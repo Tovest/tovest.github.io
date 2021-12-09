76101762f33a605a233ef7b02e08f3d3ca549253
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-09 16:46:17 +0100

    Update main.js

--- src2/main.js
+++ src2/main.js
@@ -1,15 +1,15 @@
 var canvas = new Canvas2d("canvas", new CameraOrtho(0,0,0,0,0,0));
-var snapPoint1 = SnapPointEntity.create(-50,20,30);
-var snapPoint2 = SnapPointEntity.create(-40,20,25);
+var snapPoint1 = SnapPointEntity.create(-30,20,30);
+var snapPoint2 = SnapPointEntity.create(-30,20,25);
 var snapPoint3 = SnapPointEntity.create(-30,20,20);
 var snapPoint4 = SnapPointEntity.create(-20,20,15);
 var snapPoint5 = SnapPointEntity.create(-10,20,10);
 var snapPoint6 = SnapPointEntity.create(0,20,5);
 var snapPoint7 = SnapPointEntity.create(10,20,0);
-var snapPoint8 = SnapPointEntity.create(20,20,-0);
+var snapPoint8 = SnapPointEntity.create(20,20,-5);
 var snapPoint9 = SnapPointEntity.create(30,20,-10);
-var snapPoint10 = SnapPointEntity.create(40,20,-15);
-var snapPoint11 = SnapPointEntity.create(50,20,-20);
+var snapPoint10 = SnapPointEntity.create(30,20,-15);
+var snapPoint11 = SnapPointEntity.create(30,20,-20);
 //var line = new LineEntity(snapPoint1.vertex,snapPoint2.vertex);
 var workplace = new Workplace([snapPoint1,snapPoint2,snapPoint3,snapPoint4,snapPoint5,snapPoint6,snapPoint7,snapPoint8,snapPoint9,snapPoint10,snapPoint11],canvas);
 
