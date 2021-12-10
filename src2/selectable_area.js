de5562143d92f06aa98c626ab2042f83381322ac
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-10 03:55:16 +0100

    Update selectable_area.js

--- src2/selectable_area.js
+++ src2/selectable_area.js
@@ -61,13 +61,12 @@ class TriangleArea extends SelectableArea {
 		this.y3 = undefined;
 	}
 	containsPoint(x,y) {
-		console.log(sideOfLineUnormalized(this.x1,this.y1,this.x2,this.y2,x,y));
-		console.log(sideOfLineUnormalized(this.x2,this.y2,this.x3,this.y3,x,y));
-		console.log(sideOfLineUnormalized(this.x3,this.y3,this.x1,this.y1,x,y));
+		var sumOfSides =
+			(sideOfLineUnormalized(this.x1,this.y1,this.x2,this.y2,x,y) >= 0)+
+			(sideOfLineUnormalized(this.x2,this.y2,this.x3,this.y3,x,y) >= 0)+
+			(sideOfLineUnormalized(this.x3,this.y3,this.x1,this.y1,x,y) >= 0);
 		return (
-			( sideOfLineUnormalized(this.x1,this.y1,this.x2,this.y2,x,y) >= 0 ) ==
-			( sideOfLineUnormalized(this.x2,this.y2,this.x3,this.y3,x,y) >= 0 ) ==
-			( sideOfLineUnormalized(this.x3,this.y3,this.x1,this.y1,x,y) >= 0 )
+			return (sumOfSides == 0 || sumOfSides == 3);
 		);
 	}
 }
