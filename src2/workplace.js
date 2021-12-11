13c6be6e3c79d3ee691f0c9b3d7648ed75a10c9b
Tovest <94750683+Tovest@users.noreply.github.com>
2021-12-11 01:23:12 +0100

    Update workplace.js

--- src2/workplace.js
+++ src2/workplace.js
@@ -2,6 +2,8 @@ class Workplace {
 	constructor(entityList,canvas) {
 		this.entityList = entityList;
 		this.canvas = canvas;
+		this.options = undefined;
+		this.requestHandler = undefined;
 	}
 	render() {
 		this.canvas.render(this.entityList);
